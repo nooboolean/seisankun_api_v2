84835a56b975a701d0f45b6dd5b4458d343c4e1f
nooboolean <nooboolean@i.softbank.jp>
2021-12-13 19:37:28 +0900

    main.go formatting

--- app/main.go
+++ app/main.go
@@ -1,17 +1,18 @@
 package main
- 
-import (
-  "net/http"
-  "github.com/gin-gonic/gin"
-)
- 
-func main() {
-  router := gin.Default()
 
-	v1 := router.Group("api/v2")
-  v1.GET("/hello", func(c *gin.Context) {
-    c.String(http.StatusOK, "Hello World!!")
-  })
- 
-  router.Run(":3000")
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	v2 := router.Group("api/v2")
+	v2.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!!")
+	})
+
+	router.Run(":3000")
 }
